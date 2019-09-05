0fdb306765269595fc031dd382a295186249896c
Sercan PASPAL <seco@Sercan-MacBook-Air.local>
2019-09-05 08:40:38 +0300

    Updates

---
+++ precache-manifest.9d2b442c7307638ec62f9c0eebf9d913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c49072bafdd3e5a78e7910f5ec4b51c",
+    "url": "/react-context-api-boilerplate/index.html"
+  },
+  {
+    "revision": "7e70c6254ca75cbe2ff2",
+    "url": "/react-context-api-boilerplate/static/css/main.8b405afe.chunk.css"
+  },
+  {
+    "revision": "babef24b3aab1d77b3b9",
+    "url": "/react-context-api-boilerplate/static/js/2.8f090c8f.chunk.js"
+  },
+  {
+    "revision": "7e70c6254ca75cbe2ff2",
+    "url": "/react-context-api-boilerplate/static/js/main.f1c514ff.chunk.js"
+  },
+  {
+    "revision": "bbe74a53c7eee02b8237",
+    "url": "/react-context-api-boilerplate/static/js/runtime~main.3d3bcebc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-context-api-boilerplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
